b12241612cb60dbbf55b4314f78ab8bb2e0dcdfb
dreiss <david.reiss@fu-berlin.de>
2024-12-12 13:18:08 +0100

    Complete refactoring of reweighted_dynamics.py

--- reweighted_dynamics.py
+++ reweighted_dynamics.py
@@ -14,126 +14,148 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 
 
 import numpy as np
-from utilities import plot_prob_distribution
+from utilities import ConsistentParametersClass
 
 
-class ReweightedDynamics:
-    def __init__(self, T: int, s: float, x_0=0., prob_step_up=0.5):
+class ReweightedDynamics(ConsistentParametersClass):
+    def __init__(self, T: int, s: float, x_T: int, prob_step_up: float):
+        super().__init__()
+
         # save inputs
         self.T = T
         self.s = s
+        self.x_T = x_T
+        self.prob_step_up = prob_step_up
 
-        """
-        # initialize arrays
-        self.g_prime_array = np.empty((T + 1, 2 * T + 1))
-        self.g_prime_array[:] = np.nan
+        # compute gauge transform necessary in chosen way to exactly compute reweighted dynamics
+        self.gauge_transform_g = self.calc_gauge_transform_array(T, s, x_T=x_T, prob_step_up=prob_step_up)
 
-        # calculate gauge transformation and reweighted dynamics
-        self.calc_gauge_transformation_recursively(0, 0, T, s, x_0=x_0)
-        for x in 2 * np.arange(T + 1) - T:
-            self.calc_gauge_transformation_recursively(x, T, T, s, x_0=x_0)
-        """
+        # extract partition function from gauge transforma
+        self.partition_function_Z = self.calc_partition_function(self.gauge_transform_g, T)
 
-        self.g_array = self.calc_gauge_transformation_array(T, s, x_0=x_0, prob_step_up=prob_step_up)
+        # compute reweighted dynamics from gauge transform
+        self.reweighted_dynamics_P_W = self.calc_reweighted_dynamics_array(self.gauge_transform_g, T, s,
+                                                                           x_T=x_T, prob_step_up=prob_step_up)
 
-        self.partition_function_Z = self.calc_partition_function(self.g_array, T)
 
-        # plot_prob_distribution(T, self.g_array, set_title=False, title="$g$ ", plot_complement=True)
-
-        self.P_W_array = self.calc_reweighted_dynamics(T, s, self.g_array, x_0=x_0, prob_step_up=prob_step_up)
-
-        # plot reweighted dynamics
-        plot_prob_distribution(T, self.P_W_array, set_title=False, title="$P_W$ ", plot_complement=True)
-
+    @property
+    def all_init_params_dict(self):
+        return {"T": self.T, "s": self.s, "x_0": self.x_T, "prob_step_up": self.prob_step_up}
 
 
     @staticmethod
-    def calc_weight_function(x: float, s: float, x_0=0.):
+    def calc_weight_function(x: float | np.ndarray, s: float, x_T=0) -> float | np.ndarray:
+        """
+        Calculate weight function W of (softened) constraint for rare trajectories with a specific final position.
+
+        Parameters:
+            x: final position of trajectory
+            s: softening parameter
+            x_T: required final position of rare trajectory
+
+        Returns:
+            weight function value
         """
 
-        :param x:
-        :param s:
-        :return:
-        """
-        return np.exp(- s * (x - x_0)**2)
+        assert s >= 0, "s >= 0 required"
 
-
-    @staticmethod
-    def calc_partition_function(g_array: np.ndarray, T: int):
-        return g_array[0, T]
-
-
-    def calc_gauge_transformation_recursively(self, x: int, t: int, T: int, s: float, x_0=0.) -> float:
-        """
-        Calculates gauge transformation for balanced random walk bridges with softened constraint,
-        called g' by Rose et al. ('21), by recursion
-        (balanced: prob. to go 1 step up = prob. to go 1 step down;
-        softened constraint: e^{- s x_T^2}) instead of \delta(x_T)).
-        :param x: position
-        :param t: time
-        :param T: maximal time
-        :param s: softening parameter
-        :return: g'(x, t) for parameters T and s
-        """
-        assert t <= T, "t <= T required"
-
-        if t == T:
-            g_prime_value = 1
-
-        elif t == T - 1:
-            g_prime_value = self.calc_weight_function(x + 1, s, x_0=x_0) + self.calc_weight_function(x - 1, s, x_0=x_0)
+        if s == np.inf:
+            return np.where(x == x_T, 1, 0)  # delta function
 
         else:
-            g_prime_value = (self.calc_gauge_transformation_recursively(x + 1, t + 1, T, s)
-                             + self.calc_gauge_transformation_recursively(x - 1, t + 1, T, s))
-
-        self.g_array[t, x + T] = g_prime_value
-
-        return g_prime_value
+            return np.exp(- s * (x - x_T) ** 2)
 
 
-    def calc_gauge_transformation_array(self, T: int, s: float, x_0=0., prob_step_up=0.5):
+    def calc_gauge_transform_array(self, T: int, s: float, x_T=0, prob_step_up=0.5) -> np.ndarray:
+        """
+        Calculate gauge transform g (for rare trajectories of random walk with softened constraint) via
+        iterative solution of recursion equation.
+        For details see Rose et al. 2021 New J. Phys. 23 013013, https://doi.org/10.1088/1367-2630/abd7bd,
+        App. A and in particular Eq. (A.10).
+
+        Parameters:
+            T: #time steps of random walk
+            s: softening parameter
+            x_T: required final position of rare trajectory
+            prob_step_up: probability to go 1 step up
+
+        Returns:
+            g(x, t) for parameters T and s
+        """
+
+        assert 0 <= prob_step_up <= 1, "0 <= prob_step_up <= 1 required"
+
         # initialization
         g_array = np.empty((T + 1, 2 * T + 1))
         g_array[:] = np.nan
 
+        # utility function
         def calc_g_value(x: int, t: int):
             if t == T:
                 return 1
 
             elif t == T - 1:
-                return prob_step_up * self.calc_weight_function(x + 1, s, x_0=x_0) \
-                    + (1 - prob_step_up) * self.calc_weight_function(x - 1, s, x_0=x_0)
+                return prob_step_up * self.calc_weight_function(x + 1, s, x_T=x_T) \
+                    + (1 - prob_step_up) * self.calc_weight_function(x - 1, s, x_T=x_T)
 
             else:
                 return prob_step_up * g_array[t + 1, x + 1 + T] \
                     + (1 - prob_step_up) * g_array[t + 1, x - 1 + T]
 
+        # compute gauge transform values
         for t in np.arange(T + 1)[::-1]:
             for x in np.arange(- t, t + 1, 2):
                 g_array[t, x + T] = calc_g_value(x, t)
 
-        print(g_array)
         return g_array
 
 
+    @staticmethod
+    def calc_partition_function(gauge_transform_g: np.ndarray, T: int) -> float:
+        """
+        Extract partition function Z from gauge transform.
+        For details see Rose et al. 2021 New J. Phys. 23 013013, https://doi.org/10.1088/1367-2630/abd7bd,
+        App. B and in particular Eq. (B.2).
+
+        Parameters:
+            T: #time steps of random walk
+            gauge_transform_g: array containing gauge transform values
+
+        Returns:
+            partition function Z
+        """
+
+        assert np.shape(gauge_transform_g) == (T + 1, 2 * T + 1), "gauge_transform_g has wrong shape"
+
+        return gauge_transform_g[0, T]  # corresponds to calc_g_value(x = 0, t = 0), see calc_gauge_transform_array
+
 
     @staticmethod
-    def calc_reweighted_dynamics(T: int, s: float, g_array: np.ndarray, x_0=0., prob_step_up=0.5):
+    def calc_reweighted_dynamics_array(gauge_transform_g: np.ndarray, T: int, s: float, x_T=0., prob_step_up=0.5):
         """
-        Calculates reweighted dynamics, called P_W by Rose et al. ('21)
-        :param T:
-        :param s:
-        :param g_array:
-        :return:
-        """
-        x_values = np.arange(- T + 1, T)
-        weights_array = np.ones((T, 2 * T - 1))
-        #weights_array[T - 1, :] = np.exp(- s * (x_values + 1)**2)  # weights for up-step in last step
-        weights_array[T - 1, :] = ReweightedDynamics.calc_weight_function(x_values + 1, s, x_0=x_0)
+        Calculate reweighted dynamics P_W (for rare trajectories of random walk with softened constraint)
+        via a gauge transform.
+        For details see Rose et al. 2021 New J. Phys. 23 013013, https://doi.org/10.1088/1367-2630/abd7bd,
+        App. A and in particular Eq. (A.9).
 
-        P_W_array = g_array[1:, 2:] / g_array[:-1, 1:-1] * weights_array * prob_step_up
-        # P_W_array only saves probabilities to go 1 step up (that to go 1 step down is given by normalization)
+        Parameters:
+            gauge_transform_g: array containing gauge transform values
+            T: #time steps of random walk
+            s: softening parameter
+            x_T: required final position of rare trajectory
+            prob_step_up: probability to go 1 step up
+
+        Returns:
+            array containing reweighted dynamics P_W
+        """
+
+        # compute weights for 1 step up in last time step
+        weights_array = np.ones((T, 2 * T - 1))
+        weights_array[T - 1, :] = ReweightedDynamics.calc_weight_function(np.arange(- T + 1, T) + 1, s, x_T=x_T)
+
+        # compute reweighted probabilities to go 1 step up according to Eq. (A.9) in Rose et al. 2021
+        P_W_array = gauge_transform_g[1:, 2:] / gauge_transform_g[:-1, 1:-1] * weights_array * prob_step_up
+        # P_W_array only contains probabilities to go 1 step up (that to go 1 step down is given by normalization)
 
         return P_W_array
 
