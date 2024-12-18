e3628f199b98fa85fd3cca9020fdb1c39f7dc6d1
dreiss <david.reiss@fu-berlin.de>
2024-12-18 19:08:11 +0100

    Delete function import_params_from_csv, build functions import_params_from_json5 and convert_to_and_save_latex_string, add try-except statement to function save_obj, fix bug regarding restoring object attributes and add checks for parameter consistency in function load_and_restore_obj

--- utilities.py
+++ utilities.py
@@ -13,9 +13,11 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 """
 
 
-import csv
 import time
+import json5
 import numpy as np
+import _pickle
+import sympy as sp
 from abc import ABC, abstractmethod
 from logging_config import get_logger
 
@@ -23,36 +25,39 @@ from logging_config import get_logger
 logger = get_logger("utilities.py")
 
 
-def import_params_from_csv(path: str) -> dict:
+def import_params_from_json5(path: str) -> dict:
     """
-    Import parameters from a CSV file and return them as a dictionary.
+    Import parameters from a JSON5 file and return them as a dictionary.
 
     Parameters:
-        path: path to the CSV file
+        path: path to the JSON5 file
 
     Returns:
         params: dict with parameter names and values as dict key-value pairs
     """
-
-    params = {}
-
-    with open(path, mode="r") as file:
-        reader = csv.DictReader(file)
-
-        for row in reader:
-            key, value = row["Parameter"], row["Value"]
-
-            # convert types appropriately
-            if value.isdigit():
-                params[key] = int(value)
-            elif value.replace('.', '', 1).isdigit():
-                params[key] = float(value)
-            elif value.lower() in ['true', 'false']:
-                params[key] = value.lower() == 'true'
-            else:
-                params[key] = value
-
+    with open(path, 'r') as file:
+        params = json5.load(file)
     return params
+
+
+def convert_to_and_save_latex_string(sympy_expr: sp.Expr, file_name: str, comment: str) -> None:
+    """
+    Convert sympy expression to equivalent LaTeX string and save it in txt file.
+
+    Parameters:
+        sympy_expr: sympy expression to be converted
+        file_name: name of the file to save the LaTeX string
+        comment: comment to be saved in the last line of the file
+
+    Returns:
+        None
+    """
+
+    latex_string = sp.latex(sympy_expr, mode='inline')
+
+    with open('file.txt', 'w') as file:
+        file.write(latex_string + '\n')
+        file.write(comment + '\n')
 
 
 def einsum_subscripts(*initial_indices: str, final_indices="") -> str:
@@ -122,7 +127,12 @@ def save_obj(obj, file_name: str) -> None:
     """
 
     attributes = obj.__dict__
-    np.savez(file_name, **attributes)
+
+    try:
+        np.savez(file_name, **attributes)
+
+    except _pickle.PicklingError:
+        logger.info(f"Object cannot be saved in {file_name} due to PicklingError.")
 
 
 def load_and_restore_obj(class_type, file_name: str, all_params_dict: dict):
@@ -143,6 +153,14 @@ def load_and_restore_obj(class_type, file_name: str, all_params_dict: dict):
     obj = class_type.__new__(class_type)
 
     attributes = {f: data[f] for f in data.files}
+
+    # combination of np.savez in function save_obj and np.load above leads to attributes originally of type
+    # 'int', 'float', 'str', 'bool', etc. being loaded as np.ndarray objects with a single element
+    # -> revert this
+    for key, value in attributes.items():
+        if value.size == 1:
+            attributes[key] = value.item()
+
     obj.__dict__.update(attributes)
 
     if not isinstance(obj, ConsistentParametersClass):
@@ -159,12 +177,39 @@ def load_and_restore_obj(class_type, file_name: str, all_params_dict: dict):
         logger.debug(error_message)
         raise AssertionError(error_message)
 
-    if not all(item in all_params_dict.items() for item in obj.all_init_params_dict.items()):
+    parameters_are_not_consistent = False
+
+    for key, value in obj.all_init_params_dict.items():
+        if key == "no_qubits":
+            if not (value in all_params_dict["no_qubits_list"]):
+                print(key, value)
+                parameters_are_not_consistent = True
+                break
+
+        elif key == "no_layers":
+            if not (value in all_params_dict["no_layers_list"]):
+                print(key, value)
+                parameters_are_not_consistent = True
+                break
+
+        elif not key in all_params_dict.keys():
+            print(key, value, all_params_dict[key])
+            parameters_are_not_consistent = True
+            break
+
+        elif all_params_dict[key] != value:
+            print(key, value, all_params_dict[key])
+            parameters_are_not_consistent = True
+            break
+
+    if parameters_are_not_consistent:
         error_message = ("All parameter-value pairs in all_init_params_dict of loaded object must be consistent with "
                          "parameter-value pairs in all_params_dict which contains all parameter-value pairs used for "
                          "current computations.")
 
         logger.debug(error_message)
+        logger.debug(f"all_init_params_dict of loaded object: {obj.all_init_params_dict}")
+        logger.debug(f"all_params_dict used for current computations: {all_params_dict}")
         raise AssertionError(error_message)
 
     return obj
@@ -196,7 +241,10 @@ def load_or_compute_obj(class_type, generator_function, file_name: str, all_para
             logger.info(f"File {file_name} not found.")
 
         except AssertionError:
-            logger.info(f"Loaded object of {class_type} from {file_name} cannot be restored consistently.")
+            logger.info(f"Consistent restoring of loaded object of {class_type} from {file_name} is not possible.")
+
+        except _pickle.PicklingError:
+            logger.info(f"Object of {class_type} from {file_name} cannot be loaded due to PicklingError.")
 
     # so if recompute is True or if load_and_restore_obj failed due to file with file_name not existing
     # or due to all_init_params_dict of loaded object not consistent with all_params_dict used for current computations:
@@ -216,7 +264,7 @@ class ConsistentParametersClass(ABC):
     Abstract base class for classes for which one wants to use function load_and_restore_obj (for example via function
     load_or_compute_obj) to load and restore instances of the respective class from files. The combination of this class
     and this function ensures that the parameters which had been used to initialize the now restored instance are
-    CONSISTENT with the parameters which are used in the CURRENT computations.
+    consistent with the parameters which are used in the current computations.
     """
     def __init__(self):
         pass
